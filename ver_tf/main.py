f347149bfcab9758637da3b286cf65f7e7c91dce
Osama Khallouf <50551721+Exeedo@users.noreply.github.com>
2022-07-23 10:54:09 +0300

    Update main.py

--- ver_tf/main.py
+++ ver_tf/main.py
@@ -1,1 +1,14 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
+import PIL
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import arabic_reshaper
+
+print("All imports are successful")
